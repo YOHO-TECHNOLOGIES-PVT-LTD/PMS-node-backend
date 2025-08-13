86d33b08aa9040052202f575e23b1daf60569e27
sivayoho <sivashankar.yoho@gmail.com>
2025-08-13 13:25:54 +0530

    updated

---
+++ src/controllers/Notification/index.js
@@ -0,0 +1,42 @@
+import { NotifyModel } from "../../models/Notification"
+
+export const notificationGetAll = async (req, res) => {
+    try {
+        const {
+            page = 1,
+            limit = 10,
+        } = req.query;
+        const skip = (parseInt(page) - 1) * parseInt(limit);
+
+        const notification = await NotifyModel.find({ is_deleted: false }).skip(skip).limit(parseInt(limit)).sort({ createdAt: -1 })
+        return res.status(200).json({
+            success: true,
+            message: "Notification Retrieved Successfully",
+            data: notification
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const markedAsRead = async (req, res) => {
+    try {
+        const { uuid } = req.params;
+        const updated = await NotifyModel.findOneAndUpdate(
+            { uuid: uuid },
+            { is_read: true }
+        );
+        return res.status(200).json({
+            success: true,
+            message: "Updated notification marked as read"
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
